55ef3f79e04d82f1d4abc64c77481bc6c31235fd
Butch Santos <cusantos@globe.com.ph>
2023-09-06 09:21:24 +0800

    feat: added google oauth.

---
+++ src/main/java/com/ibutch/apigateway/configs/Security.java
@@ -0,0 +1,26 @@
+package com.ibutch.apigateway.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * Created by butchsantos on 9/6/23.
+ */
+@Configuration
+public class Security {
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(authorize ->
+                        authorize
+                            .anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .oauth2Client(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
